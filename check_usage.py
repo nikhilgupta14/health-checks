679d8eea4a3d4e344f143c4b433e6c70f4bd4340
Nikhil Gupta <nikhilgupta123459@gmail.com>
2020-08-14 23:05:42 +0530

    Allow printing more than one message

--- check_usage.py
+++ check_usage.py
@@ -36,10 +36,14 @@ def main():
     checks = [
     (check_reboot, "Pending-Reboot"),
     (check_root_full, "Root partition full"),
+    (check_cpu_usage, "Not enough disk space")
     ]
+    everything_ok = True
     for check, msg in checks:
         if check():
             print(msg)
+            everything_ok = False
+        if not everything_ok:
             sys.exit(1)
 
 print("Everything is okk😂")
