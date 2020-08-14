e1e15473fbd2656dc24e938aa13ec82f7e7bc006
Nikhil Gupta <nikhilgupta123459@gmail.com>
2020-08-14 23:00:07 +0530

    Iterate over a list of checks and messages to avoid code duplication

--- check_usage.py
+++ check_usage.py
@@ -33,12 +33,14 @@ def check_root_full():
 #     print("ERROR: Not enough disk space.")
 #     sys.exit(1)
 def main():
-    if check_reboot():
-        print("Pending-Reboot")
-        sys.exit(1)
-    if check_root_full():
-        print("Root partition full")
-        sys.exit(1)
-        
+    checks = [
+    (check_reboot, "Pending-Reboot"),
+    (check_root_full, "Root partition full"),
+    ]
+    for check, msg in checks:
+        if check():
+            print(msg)
+            sys.exit(1)
+
 print("Everything is okk😂")
 sys.exit(0)
